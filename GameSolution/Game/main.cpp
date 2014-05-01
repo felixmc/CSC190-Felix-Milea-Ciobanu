363690209d48e26214f7e93b1b4b9ca312ea8aa8
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-04-30 20:52:09 -0600

    initial game setup

--- GameSolution/Game/main.cpp
+++ GameSolution/Game/main.cpp
@@ -1,7 +1,30 @@
 #include "Engine.h"
+#include "Core.h"
+
+using Core::Input;
+
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 900;
+
+bool update(float dt) {
+	dt;
+	return Input::IsPressed(Input::KEY_ESCAPE);
+}
+
+void draw(Core::Graphics& graphics) {
+	graphics.SetColor(RGB(255,255,255));
+	graphics.DrawString(SCREEN_WIDTH/2 - 50, SCREEN_HEIGHT/2 - 20, "Hello World");
+	graphics.DrawLine(10,10,400,300);
+}
 
 int main()
 {
-	Engine::sampleFunctionThatReturnsTrue();
+	Core::Init("Game Demo", SCREEN_WIDTH, SCREEN_HEIGHT);
+	Core::RegisterUpdateFn(update);
+	Core::RegisterDrawFn(draw);
+
+	Core::GameLoop();
+
+	return 0;
 }
 
